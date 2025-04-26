cd5516388f6f5d69fd212e934312a67b861a2b9f
pedja98 <predragmirkovic1998@gmail.com>
2025-04-26 19:10:34 +0200

    establish webpack module federation for catalogue

---
+++ src/CatalogueApp.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CatalogueApp = () => {
+  return <div>This is Catalogue App</div>
+}
+
+export default CatalogueApp
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { ModuleFederationPlugin } = require('webpack').container
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+      serveIndex: false,
+    },
+  },
+  output: {
+    publicPath: 'auto',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'catalogue',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './CatalogueApp': './src/CatalogueApp',
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: '^18.0.0' },
+        'react-dom': { singleton: true, requiredVersion: '^18.0.0' },
+      },
+    }),
+  ],
+}
